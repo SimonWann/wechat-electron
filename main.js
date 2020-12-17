2649122ccafb98f0acc6f7601100a50db1fc8825
unknown <2252442507@qq.com>
2020-12-17 13:17:28 +0800

    a

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow, Tray, Menu } = require('electron')
+const path = require('path')
+const neteaseApi = require('./app')
+
+let tray = null
+const icon = path.join(__dirname,'/wechat.exe.ico')
+
+
+
+function createWindow () {   
+    // 创建浏览器窗口
+    const win = new BrowserWindow({
+      width: 1100,
+      height: 720,
+      icon: icon,
+      autoHideMenuBar: true,
+      webPreferences: {
+        nodeIntegration: true
+      },
+    })
+    win.loadURL("https://wx.qq.com/")
+    // 并且为你的应用加载index.html
+    tray = new Tray(icon)
+    const contextMenu = Menu.buildFromTemplate([
+      { label: '显示主窗口', click() {
+        win.show()
+      } },
+      { label: '退出', role: 'quit' }
+    ])
+    tray.setToolTip('wechat-electron')
+    tray.setContextMenu(contextMenu)
+    
+  }
+  
+  app.whenReady().then(() => {
+    
+    createWindow()
+    
+  })
+  app.on('active', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+        
+    }
+  })
